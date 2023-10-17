456e8b698f096c3e0ab27d34454d4a74c5a5fbb1
brookefol <bfoley2026@gmail.com>
2023-10-17 16:08:52 -0400

    ehhehe

---
+++ sandbox.js
@@ -0,0 +1,39 @@
+let age = 25;
+let year = 2023;
+
+console.log(age, year); // system.out.println of java script
+
+let firstName = "Elle";
+let lastName = "Kim"; 
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+console.log(fullName.length);
+
+console.log(fullName[2]);
+
+console.log(fullName.toUpperCase())
+
+// Conditionals
+let date = 17;
+if (date === "17") { // three equal signs checks for typing
+    console.log("yay!");
+}
+
+// functions
+function add (a, b) {
+    return a + b;
+}
+
+add(1, 2);
+
+function printHi (name) {
+    return "hi " + name;
+}
+
+function printBye(func) {
+    return func ("Jolene") + " bye";
+}
+
+console.log(printBye(printHi));
